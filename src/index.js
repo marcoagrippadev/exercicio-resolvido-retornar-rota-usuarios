c77cc59d771586a0dd75c7a7222926c4f6c66a1b
marcoagrippadev <ma.agrippa@gmail.com>
2023-08-06 12:22:19 -0300

    v2 utilizando funcao de callback

--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@ const express = require("express");
 const app = express();
 
 const retornarUsuarios = [
-    {id: 11, nome: "joao", idade:23},
-    {id: 2, nome: "maria", idade:18},
-    {id: 4, nome: "ana", idade:30},
-    {id: 1, nome: "rodrigo", idade:17},
-    {id: 123, nome: "rodrigo", idade:17},
+  { id: 11, nome: "joao", idade: 23 },
+  { id: 2, nome: "maria", idade: 18 },
+  { id: 4, nome: "ana", idade: 30 },
+  { id: 1, nome: "rodrigo", idade: 17 },
+  { id: 123, nome: "rodrigo", idade: 17 },
+];
 
-]
+function avisarInicializacaoDoServidor() {
+  console.log("Sevidor Inicializado Na Porta 3000");
+}
 
 app.get("/usuarios", function (requisicao, resposta) {
   resposta.send(retornarUsuarios);
 });
 
-app.listen(3000);
+app.listen(3000, avisarInicializacaoDoServidor);
